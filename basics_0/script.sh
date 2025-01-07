f9f938a7e984755c6ca365618d58e9c9da95e0f8
zyclamenace <gemicieneshakki@gmail.com>
2025-01-07 12:32:20 +0100

    Fix: answer typo

--- basics_0/script.sh
+++ basics_0/script.sh
@@ -2,38 +2,38 @@
 # Ce script génère automatiquement les fichiers de réponses pour les questions à choix multiples
 
 # 0-OSI_model
-echo "#!/bin/env bash
+echo "
 # Ce script contient les réponses pour les questions sur le modèle OSI
 2
 2" > 0-OSI_model
 
 # 1-types_of_network
-echo "#!/bin/env bash
+echo "
 # Ce script contient les réponses pour les questions sur les types de réseaux
 3
 2
 1" > 1-types_of_network
 
 # 2-MAC_and_IP_address
-echo "#!/bin/env bash
+echo "
 # Ce script contient les réponses pour les questions sur les adresses MAC et IP
 2
 1" > 2-MAC_and_IP_address
 
 # 3-UDP_and_TCP
-echo "#!/bin/env bash
+echo "
 # Ce script contient les réponses pour les questions sur UDP et TCP
 1
 2
 1" > 3-UDP_and_TCP
 
 # 4-TCP_and_UDP_ports
-echo '#!/bin/env bash
+echo '#!/usr/bin/env bash
 # Ce script affiche les ports en écoute et les programmes associés
 netstat -tuln | grep LISTEN' > 4-TCP_and_UDP_ports
 
 # 5-is_the_host_on_the_network
-echo '#!/bin/env bash
+echo '#!/usr/bin/env bash
 # Ce script vérifie si un hôte est sur le réseau en utilisant ping
 if [ "$#" -eq 0 ]; then
     echo "Usage: 5-is_the_host_on_the_network {IP_ADDRESS}"
